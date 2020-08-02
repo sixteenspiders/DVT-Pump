e6d10eb070b295dbc427e66241a9660de3b5aa0f
sixteenspiders <kimivaruchi@outlook.com>
2020-08-02 18:18:41 +0530

    Add ui_set_led function

--- DVT/aa_ui.ino
+++ DVT/aa_ui.ino
@@ -5,3 +5,18 @@
  *******************************************************************************/
 
 //This file handles all User interactions through LEDs and buttons
+
+/*
+Tasks
+Create a function to get button presses
+Create led ids and led state definitions
+
+Complete
+Create a function to set led state
+ 
+*/
+
+
+void ui_set_led (char led_id, bool led_state) {
+    digitalWrite(led_id, led_state);
+}
