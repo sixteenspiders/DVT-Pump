8d0d0946ff740d70bab33ee67a5cf137b31db687
sixteenspiders <kimivaruchi@outlook.com>
2020-08-16 00:27:02 +0530

    Decorate aa_prr.ino file and add comments

--- DVT/aa_prr.ino
+++ DVT/aa_prr.ino
@@ -1,19 +1,26 @@
 //This file contains functions to handle pressure sensor
 
-/*
-TODO
---add function to take single measurement
-add function to take 50 or so measurements and return average
-add function to convert adc to prr
- */
+/********************************************************************************
+ *                                                                              *
+ *                                    Defines                                   *
+ *                                                                              *
+ *******************************************************************************/
 
 #define avg_readings 50
 
+/********************************************************************************
+ *                                                                              *
+ *                                   Functions                                  *
+ *                                                                              *
+ *******************************************************************************/
+
+// read single pressure value
 int prr_read_pressure (char sensor_id) {
     //read pressure for that sensor and return
     return analogRead(sensor_id);
 }
 
+// read many pressure values and return average
 int prr_avg_pressure (char sensor_id) {
     for (int i = 0; i < avg_readings; i++) {
         sum = prr_read_pressure(sensor_id);
@@ -22,7 +29,8 @@ int prr_avg_pressure (char sensor_id) {
     return sum/avg_readings;
 }
 
-
+// convert raw adc value to pressure mmHg
 int prr_convert_pressure (int adc_val) {
+    //convert pressure here
     return 1;
 }
